38fb0c70c5ef390c4032709da38071f2b0ae478b
陈梦恬 <mengtianchen@s-MacBook-Air.lan>
2023-06-20 14:10:35 -0700

    418

---
+++ .history/app_20230620140412.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+
+app.set('view engine')
+
+app.get('/', (req, res) => {
+    res.send('HELLO FROM YELP CAMP!')
+})
+app.listen(3000, () => {
+    console.log('Serving on port 3000')
+})
---
+++ .history/app_20230620140449.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const path = require('p')
+
+app.set('view engine', 'ejs');
+
+
+app.get('/', (req, res) => {
+    res.send('HELLO FROM YELP CAMP!')
+})
+app.listen(3000, () => {
+    console.log('Serving on port 3000')
+})
